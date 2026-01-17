5a6e362b119651d0e0bb2c719edb984ebd5b4c20
RHZIZA Mohamed <109476153+Medrhz@users.noreply.github.com>
2026-01-17 22:55:42 +0100

    first commit

---
+++ src/main/java/com/hackathon/springallcoreconcepts/SpringAllCoreConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.springallcoreconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllCoreConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAllCoreConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/springallcoreconcepts/SpringAllCoreConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.springallcoreconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllCoreConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
